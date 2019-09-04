a17d5d423eb21f6ae356a973b8be13b24fbdf953
edillenb <elio.dillenberg@gmail.com>
2019-09-04 13:05:13 +0200

    edillenb - implemented changes to result.c

--- srcs/result.c
+++ srcs/result.c
@@ -28,24 +28,22 @@ void       result(t_env *env)
           env->cr_path = env->cr_path == 1 ? 0 : 1;
         while (cr != NULL)
         {
-            //looping while we still have paths
-            cr = cr->tail_path->prev_room;
+            //looping while we still have paths to explore
+            cr = cr->tail_path;
             while (cr->prev_room != NULL)
             {
-                //looping while there are rooms to explore within this path
+                //looping through the path starting from end until we reach first room before start
                 if (cr->ant)
                 {
-                    cr->next_room->ant = cr->ant;
-                    cr->ant = 0;
-                    ft_printf("L%d-%s ", cr->next_room->ant, env->rm_tab[cr->next_room->index]->name);
-                    if (cr->next_room->next_room == NULL)
+                    ft_printf("L%d-%s ", cr->ant, env->rm_tab[cr->index]->name);
+                    if (cr->next_room == NULL)
                         env->ants_end++;
+                    else
+                        cr->next_room->ant = cr->ant;
+                    cr->ant = 0;
                 }
                 if (cr->prev_room->len && env->ants_end < env->nt_rm[0] && env->next_ant <= env->nt_rm[0])
-                {
                     cr->ant = env->next_ant++;
-                    ft_printf("L%d-%s ", cr->ant, env->rm_tab[cr->index]->name);
-                }
                 cr = cr->prev_room;
             }
             cr = cr->next_path;
